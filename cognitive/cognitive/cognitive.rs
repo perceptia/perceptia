ef733db38d7a72b225b4be5d768677028bf16244
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2017-05-19 19:34:48 +0200

    Create cognitive

---
+++ cognitive/cognitive/cognitive.rs
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+// the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+//! Curate package for crates related to building display compositors or window managers.
+//!
+//! After crates reexported here mature they all will be moved to new repository.
+
+#![warn(missing_docs)]
+
+pub extern crate timber;
+pub extern crate dharma;
